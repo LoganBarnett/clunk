a7d71fdd80d81eb45f14d7698243ae5cdb79838b
LoganBarnett <logustus@gmail.com>
2024-05-22 14:31:23 -0700

    add Rust initial project cruft

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// See https://www.reddit.com/r/rust/comments/e4vje7/comment/f9g8myc/ for
+// material on how to do plugins using Rust - but this can work for non-Rust
+// applications.
+//
+// For our first iteration, this will be hard-coded.  Plugins can be broken out
+// once we've achieved proof of concept.
+fn main() {
+  println!("Hello, world!");
+}
